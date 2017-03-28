0b248c4b90a97d7db120c02abed40dcc38592b1a
Damilare Joseph <dre4success@live.com>
2017-03-28 11:14:57 +0100

    Exception Error handling

--- www/test.php
+++ www/test.php
@@ -4,4 +4,13 @@ define('DBNAME', 't_online');
 define('DBUSER', 'root');
 define('DBPASS', 'dre');
 
-$conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+try {
+	# prepare a pdo instance
+	$conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+	#set verbose error modes
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}
