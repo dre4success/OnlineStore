d3abc8f4f9677d260adefe6ac9fcaa2d39da1cc6
Damilare Joseph <dre4success@live.com>
2017-03-28 17:31:18 +0100

    uploading file to folder

--- www/test.php
+++ www/test.php
@@ -38,6 +38,19 @@ if(array_key_exists('save', $_POST)) {
 		$errors[] = "invalid file type";
 	}
 
+	#generate random number to append
+	$rnd = rand(0000000000, 9999999999);
+
+	#strip filename for spaces
+	$strip_name = str_replace(" ", "_", $_FILES['pic']['name']);
+
+	$filename = $rnd.$strip_name;
+	$destination = 'uploads/'.$filename;
+
+	if(!move_uploaded_file($_FILES['pic']['tmp_name'], $destination)) {
+		$errors[] = "file upload failed";
+	}
+
 	if(empty($errors)) {
 		echo "done";
 	} else {
