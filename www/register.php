2f127e015f29f28b94db3ba65d722594ae509ee5
Damilare Joseph <dre4success@live.com>
2017-03-29 10:57:32 +0100

    adding style to template

--- www/register.php
+++ www/register.php
@@ -1,33 +1,37 @@
+<?php
+		# include header
+		include 'includes/header.php';
+?>
 	<div class="wrapper">
 		<h1 id="register-label">Admin Register</h1>
 		<hr>
 		<form id="register"  action ="register.php" method ="POST">
 			<div>
-				<label>first name:
+				<label>first name:</label>
 				<input type="text" name="fname" placeholder="first name">
 				</label>
 			</div>
 			<div>
-				<label>last name:	
+				<label>last name:</label>
 				<input type="text" name="lname" placeholder="last name">
 				</label>
 			</div>
 
 			<div>
-				<label>email:
+				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 				</label>
 			</div>
 			<div>
-				<label>password:
+				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
-				</label>
+				
 			</div>
  
 			<div>
-				<label>confirm password:	
+				<label>confirm password:</label>	
 				<input type="password" name="pword" placeholder="password">
-				</label>
+				
 			</div>
 
 			<input type="submit" name="register" value="register">
@@ -35,3 +39,8 @@
 
 		<h4 class="jumpto">Have an account? <a href="login.php">login</a></h4>
 	</div>
+
+	<?php
+		# inlude footer
+		include 'includes/footer.php';
+	?>
