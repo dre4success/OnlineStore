b379f1f9f6cdd8f011a32009fcc2149b8ac10754
Damilare Joseph <dre4success@live.com>
2017-03-29 11:52:32 +0100

    Making changes to the title of login and register

--- www/login.php
+++ www/login.php
@@ -1,4 +1,5 @@
 <?php
+		$page_title = "Login";
 		include 'includes/header.php';
 ?>
 
--- www/register.php
+++ www/register.php
@@ -1,4 +1,6 @@
 <?php
+		$page_title = "Register";
+		
 		# include header
 		include 'includes/header.php';
 ?>
