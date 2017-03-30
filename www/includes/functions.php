ae8c033b0083a4a5cecf6cce26ada7cb9d139c23
Damilare Joseph <dre4success@live.com>
2017-03-30 18:00:50 +0100

    creat function for if email exists

--- www/includes/functions.php
+++ www/includes/functions.php
@@ -3,12 +3,13 @@
 
 		function doAdminRegister($dbconn, $input) {
 
-			#hashing password
+			# hashing password
 			$hash = password_hash($input['password'], PASSWORD_BCRYPT);
 
-			#insert preparation
+			# prepared statement
 			$stmt = $dbconn->prepare("INSERT INTO admin(firstname, lastname, email, hash) VALUES(:fn, :ln, :e, :h)");			
 
+			#bind params
 			$data = [
 					':fn' => $input['fname'],
 					':ln' => $input['lname'],
@@ -16,9 +17,28 @@
 					':h' => $hash
 					];
 
+			# execute prepared statement
 			$stmt->execute($data);
 
 
+		}
+
+		function doesEmailExist($dbconn, $email) {
+			$result = false;
+
+			$stmt = $dbconn->prepare("SELECT email FROM admin WHERE email=:e");
+
+			#bind params
+			$stmt->bindParam(":e", $email);
+			$stmt->execute();
+
+			# get number of rows returned
+			$count = $stmt->rowCount();
+
+			if($count > 0) {
+				$result = true;
+			}
+			return $result;
 		}
 
 ?>		
