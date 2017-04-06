73702ee2a6adbe8e2c0699dcacec59b0aa394fc1
Damilare Joseph <dre4success@live.com>
2017-04-06 15:03:33 +0100

    make view_product page slimmer

--- www/view_product.php
+++ www/view_product.php
@@ -15,44 +15,7 @@
 
 		authenticate();
 
-	/*	if(isset($_GET['del'])) {
-
-			if($_GET['del'] = "delete") {
-				delPro($conn, $_GET['book_id']);
-			}
-		} */
-
-		/*if(array_key_exists('edit', $_POST)){
-			$clean = array_map('trim', $_POST);
-			editPro($conn, $clean);
-		}
-?>
-		
-
-				
-				<?php
-					if(isset($_GET['action'])){
-
-						if($_GET['action'] = "edit"){
-
-				?>
-
-			<h3>Edit Product</h3>
-
-				<form id="register" action ="view_product.php" method ="POST">
-
-				<input type="text" name="til" placeholder="Book Title" value="<?php echo $_GET['title']; ?>">
-				<input type="text" name="auth" placeholder="Book Author" value="<?php echo $_GET['author']; ?>">
-				<input type="text" name="pri" placeholder="Price" value="<?php echo $_GET['price']; ?>">
-				<input type="text" name="yer" placeholder="Year" value="<?php echo $_GET['year_of_publication']; ?>">
-				<input type="text" name="bn" placeholder="ISBN" value="<?php echo $_GET['isbn']; ?>">
-				<input type="hidden" name="bk" value="<?php echo $_GET['book_id']; ?>">
-				<input type="submit" name="edit" value="edit">
-
-				</form>
-					<?php
-				}
-			} */
+	
 			?>
 				<div class="wrapper">
 				<h1 id="register-label">View Product</h1>
