c2038225df08a3cddbb2360f3142464fb025734f
Damilare Joseph <dre4success@live.com>
2017-04-07 23:41:15 +0100

    starting work on login

---
+++ www/frontend/user_login.php
@@ -0,0 +1,15 @@
+<?php
+		# title
+		$page_title = "Login";
+
+		# body id for css
+		$body_id = "login";
+
+		# load db connection
+		include '../includes/db.php';
+
+		# load function
+		include '../includes/functions.php';
+
+		# include header
+		include '../includes/user_header.php';
