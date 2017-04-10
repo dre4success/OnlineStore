f6e020a211f015e89efa922d81cd3288578d27b3
Damilare Joseph <dre4success@live.com>
2017-04-10 21:46:26 +0100

    clean catalogue up

--- www/frontend/catalogue.php
+++ www/frontend/catalogue.php
@@ -37,18 +37,6 @@
         			<a href="catalogue.php?cat_id=<?php echo $id; ?>&cat_name=<?php echo $cat; ?>"><li class="category"><?php echo $cat; ?></li></a>
 
         			<?php } ?>
-       <!-- <a href="#"><li class="category">HTML</li></a>
-        <a href="#"><li class="category">History</li></a>
-        <a href="#"><li class="category">Literature</li></a>
-        <a href="#"><li class="category">Mathematics</li></a>
-        <a href="#"><li class="category">Engineering</li></a>
-        <a href="#"><li class="category">Politics</li></a>
-        <a href="#"><li class="category">Music</li></a>
-        <a href="#"><li class="category">Literature</li></a>
-        <a href="#"><li class="category">Mathematics</li></a>
-        <a href="#"><li class="category">Engineering</li></a>
-        <a href="#"><li class="category">Politics</li></a>
-        <a href="#"><li class="category">Music</li></a> -->
       </ul>
     </div>
   </div>
@@ -75,34 +63,7 @@
 
         </li>
         	<?php } ?>
-      <!--  <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$90</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$250</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$50</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$250</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$50</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$125</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$90</p></div>
-        </li> -->
+
       </ul>
       <div class="actions">
         <button class="def-button previous">Previous</button>
@@ -110,26 +71,34 @@
       </div>
     </div>
     <div class="recently-viewed-books horizontal-book-list">
-      <h3 class="header">Recently Viewed</h3>
+      <h3 class="header">&nbsp;&nbsp;&nbsp;&nbsp; Recently Viewed</h3>
       <ul class="book-list">
         <div class="scroll-back"></div>
         <div class="scroll-front"></div>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$250</p></div>
+
+        <?php 
+
+      $recent = "Recently-Viewed-Items";
+
+      $stmt = $conn->prepare("SELECT * FROM books WHERE flag=:tr");
+
+      $stmt->bindParam(':tr', $recent);
+
+      $stmt->execute();  
+
+                      
+              while($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>  
+              <li class="book">        
+          <a href="<?php echo "preview.php?book_id=".$row['book_id']?>"><div class="book-cover" style="background: url('../<?php echo $row['file_path']; ?>');
+                      background-size: cover;
+                      background-position: center;
+                      background-repeat: no-repeat;"></div></a>
+          <div class="book-price"><p><?php echo $row['price']; ?></p></div>
+              
+
         </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$50</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$125</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$90</p></div>
-        </li>
+          <?php } ?>
+       
       </ul>
     </div>
     
