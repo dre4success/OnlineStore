25f35eab9437ea0a6bd7611969217813065c122a
Damilare Joseph <dre4success@live.com>
2017-04-10 19:11:25 +0100

    setting up catalogue

---
+++ www/frontend/catalogue.php
@@ -0,0 +1,19 @@
+<?php
+		
+    session_start();
+    # title
+		$page_title = "Catalogue";
+
+		# body id for css
+		$body_id = "catalogue";
+
+		# load db connection
+		include '../includes/db.php';
+
+		# load function
+		include '../includes/functions.php';
+
+		# include header
+		include '../includes/user_header.php'; 
+
+?>
