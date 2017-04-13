3b33f7db46add566d191ccddd6ba0d059d9e8f74
Damilare Joseph <dre4success@live.com>
2017-04-13 18:14:37 +0100

    work on checkout

---
+++ www/frontend/checkout.php
@@ -0,0 +1,19 @@
+<?php
+		
+   
+    # title
+		$page_title = "Checkout";
+
+		# body id for css
+		$body_id = "checkout";
+
+		# load db connection
+		include '../includes/db.php';
+
+		# load function
+		include '../includes/functions.php';
+
+		# include header
+		include '../includes/user_header.php'; 
+
+?>
