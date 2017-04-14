b53417f11ced152679812e67c44ff0a891c8a501
Damilare Joseph <dre4success@live.com>
2017-04-14 06:51:42 +0100

    cleaning up page

--- www/frontend/cart.php
+++ www/frontend/cart.php
@@ -69,51 +69,7 @@
           </td>
         </tr>
         		<?php } ?>
-       <!-- <tr>
-          <td><div class="book-cover"></div></td>
-          <td><p class="book-price">$150</p></td>
-          <td><p class="quantity">2</p></td>
-          <td><p class="total">$300</p></td>
-          <td>
-            <form class="update">
-              <input type="number" class="text-field qty">
-              <input type="submit" class="def-button change-qty" value="Change Qty">
-            </form>
-          </td>
-          <td>
-            <a href="#" class="def-button remove-item">Remove Item</a>
-          </td>
-        </tr>
-        <tr>
-          <td><div class="book-cover"></div></td>
-          <td><p class="book-price">$300</p></td>
-          <td><p class="quantity">2</p></td>
-          <td><p class="total">$600</p></td>
-          <td>
-            <form class="update">
-              <input type="number" class="text-field qty">
-              <input type="submit" class="def-button change-qty" value="Change Qty">
-            </form>
-          </td>
-          <td>
-            <a href="#" class="def-button remove-item">Remove Item</a>
-          </td>
-        </tr>
-        <tr>
-          <td><div class="book-cover" style="background: url('img/4.jpg');background-size: contain;background-position: center;background-repeat: no-repeat;"></div></td>
-          <td><p class="book-price">$50</p></td>
-          <td><p class="quantity">5</p></td>
-          <td><p class="total">$250</p></td>
-          <td>
-            <form class="update">
-              <input type="number" class="text-field qty">
-              <input type="submit" class="def-button change-qty" value="Change Qty">
-            </form>
-          </td>
-          <td>
-            <a href="#" class="def-button remove-item">Remove Item</a>
-          </td>
-        </tr> -->
+     
       </tbody>
     </table>
     <div class="cart-table-actions">
@@ -124,7 +80,7 @@
         <a href="#"><p>2</p></a>
         <a href="#"><p>3</p></a>
       </div>
-      <a href="checkout.html"><button class="def-button checkout">Checkout</button></a>
+      <a href="checkout.php"><button class="def-button checkout">Checkout</button></a>
     </div>
     
   </div>
