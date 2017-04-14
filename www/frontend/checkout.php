1fec2b9565715233ccf3fadec1e40e6f74d0605f
Damilare Joseph <dre4success@live.com>
2017-04-14 06:52:16 +0100

    beginning checkout

--- www/frontend/checkout.php
+++ www/frontend/checkout.php
@@ -17,3 +17,25 @@
 		include '../includes/user_header.php'; 
 
 ?>
+
+	<!-- main content starts here -->
+  <div class="main">
+    <div class="checkout-form">
+      <form class="def-modal-form">
+        <div class="total-cost">
+          <h3>$2000 Total Purchase</h3>
+        </div>
+        <div class="cancel-icon close-form"></div>
+        <label for="login-form" class="header"><h3>Checkout</h3></label>
+        <input type="text"  class="text-field phone" placeholder="Phone Number">
+        <input type="text" name="addy" class="text-field address" placeholder="Address">
+        <input type="text" name="code" class="text-field post-code" placeholder="Post Code">
+        <input type="submit" name="chkt" class="def-button checkout" value="Checkout">
+      </form>
+    </div>
+  </div>
+
+   <?php
+  		#footer
+  		include '../includes/front_footer.php';
+  ?>
