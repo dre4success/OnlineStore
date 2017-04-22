3c5e0aa28ec07c848b65b61616ff4ba90ed0865f
Damilare Joseph <dre4success@live.com>
2017-04-22 12:25:36 +0100

    refactoring some methods

--- www/includes/class.Checkout.php
+++ www/includes/class.Checkout.php
@@ -1,15 +1,20 @@
 <?php
+
+	# To Insert Into Checkout Table after Getting Total amount and Quantity
 	
 	class Checkout {
 
 		private $total;
 		private $totalPur;
 		private $quantity;
-		private $tq = 0;
+		private $tq = 0; //Total Quantity
 		private $sub; //Price From Book Table Without the Dollar Sign
-		
+		private $stmt;
 
-			public function GetItemPrice($dbconn, $bkid) {
+
+
+			# Method To Get Price From Book Table
+			private function GetItemPrice($dbconn, $bkid) {
 				$statement = $dbconn->prepare("SELECT * FROM books WHERE book_id=:bi");
 				$statement->bindParam(':bi', $bkid);
 				$statement->execute();
@@ -20,13 +25,25 @@
 			}
 
 
-			# method to get total purchase
-			public function getTotal($dbconn, $userID){
-				$stmt = $dbconn->prepare("SELECT * FROM cart WHERE user_id=:id");
-				$stmt->bindParam(':id', $userID);
-				$stmt->execute(); 
 
-				while($row = $stmt->fetch(PDO::FETCH_ASSOC)) { 
+			# Method To Select From Cart Table
+			private function SelectFromCart($dbconn, $userID){
+
+				$this->stmt = $dbconn->prepare("SELECT * FROM cart WHERE user_id=:id");
+				$this->stmt->bindParam(':id', $userID);
+				$this->stmt->execute();
+
+			}
+
+
+
+			# Method To Get Total Purchase
+			public function getTotal($dbconn, $userID){
+		
+				$this->SelectFromCart($dbconn, $userID);
+
+				while($row = $this->stmt->fetch(PDO::FETCH_ASSOC)) { 
+
 					$this->GetItemPrice($dbconn, $row['book_id']);
 
 					$this->total = $this->sub * $row['quantity'];
@@ -36,33 +53,22 @@
 				return $this->totalPur;
 			}	
 
-	
-
-
-		# get total if user is not logged in
-		function getTotalTempCart($dbconn) {
-
-			$stmt = $dbconn->prepare("SELECT * FROM temp_cart");
 			
-				$stmt->execute();
+			
+			# method for counting quantity in cart
+			public function quantity($dbconn, $userID){
 
-				while($row = $stmt->fetch(PDO::FETCH_ASSOC)) { 
+				$this->SelectFromCart($dbconn, $userID);
 
-				$statement = $dbconn->prepare("SELECT * FROM books WHERE book_id=:bi");
-				$statement->bindParam(':bi', $row['book_id']);
-				$statement->execute();
+				while ($row = $this->stmt->fetch(PDO::FETCH_ASSOC)) {
+						
+						$this->quantity = $row['quantity'];
 
-				$rowBook = $statement->fetch(PDO::FETCH_ASSOC);
-				$sub = substr($rowBook['price'], 1);
-
-				$this->total = $sub * $row['quantity'];
-
-				$this->totalPur += $this->total; 
-		
+						$this->tq += $this->quantity;
 				}
+					return $this->tq;
+			}
 
-			return $this->totalPur;
-		}
 
 
 			# method to insert into checkout
@@ -83,41 +89,5 @@
 				redirect("index.php?msge=Thank You very much for using our service, Your Goods Will be shipped to you within 2days");
 			}
 
-
-
-			# method for counting quantity in cart
-			public function quantity($dbconn, $userID){
-
-				$stmt = $dbconn->prepare("SELECT quantity FROM cart WHERE user_id=:id");
-				$stmt->bindParam(':id', $userID);
-
-				$stmt->execute();
-
-				while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-						
-						$this->quantity = $row['quantity'];
-
-						$this->tq += $this->quantity;
-				}
-					return $this->tq;
-			}
-
-
-			# method for counting quantity when no user is logged in
-			public function quantitynotID($dbconn){
-
-				$stmt = $dbconn->prepare("SELECT quantity FROM temp_cart");
-				//$stmt->bindParam(':id', $userID);
-
-				$stmt->execute();
-
-				while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-						
-						$this->quantity = $row['quantity'];
-
-						$this->tq += $this->quantity;
-				}
-					return $this->tq;
-			}
 
 }
