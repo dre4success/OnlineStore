561a41fc1003038836487ca02ed6f1bc498da30d
Damilare Joseph <dre4success@live.com>
2017-04-24 15:22:15 +0100

    ongoing work

--- www/includes/class.Pagination.php
+++ www/includes/class.Pagination.php
@@ -1,4 +1,26 @@
 <?php
 		
 		# Creating a Pagination Class
+		
+		class Pagination{
+
+			private $page;
+			private $bookPerPage = 4;
+			private $offset;
+
+				private function query($dbconn) {
+					if(isset($_GET['page'])) {
+						$this->page = $_GET['page'];
+					} else
+					{
+						$this->page = 1;
+					}
+					$this->offset = ($this->page - 1) * $this->bookPerPage;
+
+					$stmt = $conn->prepare("SELECT * FROM books WHERE category_id=:id LIMIT $this->offset, $this->bookPerPage");
+					$stmt->bindParam(':id', $_GET['cat_id']);
+
+					$stmt->execute();
+				}
+		}
 ?>
