3035f5794954bbdb9ac96c7f620fc076fb8e45cb
Damilare Joseph <dre4success@live.com>
2017-04-25 00:57:14 +0100

    work in progress

--- www/includes/class.Pagination.php
+++ www/includes/class.Pagination.php
@@ -7,8 +7,10 @@
 			private $page;
 			private $bookPerPage = 4;
 			private $offset;
+			private $totalPages;
 
-				private function query($dbconn) {
+				public function __construct(){
+
 					if(isset($_GET['page'])) {
 						$this->page = $_GET['page'];
 					} else
@@ -16,11 +18,28 @@
 						$this->page = 1;
 					}
 					$this->offset = ($this->page - 1) * $this->bookPerPage;
+				}
 
-					$stmt = $conn->prepare("SELECT * FROM books WHERE category_id=:id LIMIT $this->offset, $this->bookPerPage");
+				public function query($dbconn, $catid) {
+					
+
+					$stmt = $dbconn->prepare("SELECT * FROM books WHERE category_id=:id LIMIT $this->offset, $this->bookPerPage");
+					$stmt->bindParam(':id', $catid);
+
+					$stmt->execute();
+				}
+
+				public function all($dbconn) {
+
+
+					$stmt = $dbconn->prepare("SELECT * FROM books WHERE category_id=:id");
 					$stmt->bindParam(':id', $_GET['cat_id']);
 
 					$stmt->execute();
+
+					$count = $stmt->rowCount();
+
+					$this->totalPages = ceil($count/$this->bookPerPage);
 				}
 		}
 ?>
