854f8622f1db95f12880c0013925e13705d7818d
Damilare Joseph <dre4success@live.com>
2017-04-29 19:33:06 +0100

    functionality for date

--- www/includes/front_footer.php
+++ www/includes/front_footer.php
@@ -1,6 +1,8 @@
+ 
+
  <!-- footer starts here-->
   <div class="footer">
-    <p class="copyright">&copy; copyright 2017</p>
+    <p class="copyright">&copy; copyright <?php echo date(" Y"); ?></p>
   </div>
 </body>
 </html>
